e38d737a663993358ccd3879eb2a3e486f1f4f2e
usenkok@windows <usenkok@oregonstate.edu>
2020-02-06 21:23:42 -0600

    initial layout
    Co-authored-by: Gregory D. Stula <stulag@oregonstate.edu>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,55 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar,Image } from 'react-bootstrap'
+import {Navbar, Form, Button} from 'react-bootstrap'
 import alpaca from './alpaca-cute.jpeg'
 import "./styles.css"
 
 
 function AlpacaBar() {
     return (
+        <div className="alpacaBar">
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">Alpaca Sponsoring</Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Collapse className="justify-content-end"> 
         <Navbar.Text>
-        Signed in as: <a href="#login">Alpaca Sponsor</a>
+        Signed in as: <a href="#login">Guest</a>
         </Navbar.Text>
         </Navbar.Collapse>
         </Navbar>
+        </div>
     )
 }
 
-function Header(props) {
+function Title(props) {
     return (
-        <h1>{props.title}</h1>
+        <div className="alpacaTitle">
+        <h1>{props.children}</h1>
+        </div>
+    )
+}
+
+function Login() {
+    return (
+        <div className="login">
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control size="sm" type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control size="sm" type="password" placeholder="Password" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Sign up!
+  </Button>
+</Form>
+</div>
     )
 }
 
@@ -30,8 +58,9 @@ export default () => {
     return (
     <div>
         <AlpacaBar />
-        <Header title="Alpaca"/>
+        <Title>Sponsor an Alpaca</Title>
         <img src={alpaca} style={{height: "10%" }} />
+        <Login />
     </div>
     )
 }
